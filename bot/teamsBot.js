dbaf0797f6ad8e2a6c278dd6bb0f3b7e34440e92
Priyadarshi Ghosh <prgh@microsoft.com>
2021-10-18 20:32:22 +0530

    SDP Oncall genie

---
+++ bot/teamsBot.js
@@ -0,0 +1,114 @@
+const { TeamsActivityHandler, CardFactory, TurnContext} = require("botbuilder");
+const rawWelcomeCard = require("./adaptiveCards/learn.json");
+const rawLearnCard = require("./adaptiveCards/learn.json");
+const rawEscalationsCard = require("./adaptiveCards/Escalations.json");
+const rawTBACard = require("./adaptiveCards/TBA.json");
+const rawEBACard = require("./adaptiveCards/EBA.json");
+const rawSMECard = require("./adaptiveCards/SME.json");
+const ACData = require("adaptivecards-templating");
+
+class TeamsBot extends TeamsActivityHandler {
+  constructor() {
+    super();
+
+    // record the likeCount
+    this.likeCountObj = { likeCount: 0 };
+
+    this.onMessage(async (context, next) => {
+      console.log("Running with Message Activity.");
+      let txt = context.activity.text;
+      const removedMentionText = TurnContext.removeRecipientMention(
+        context.activity
+      );
+      if (removedMentionText) {
+        // Remove the line break
+        txt = removedMentionText.toLowerCase().replace(/\n|\r/g, "").trim();
+      }
+
+      // Trigger command by IM text
+      switch (txt) {
+        case "hi": {
+          const card = this.renderAdaptiveCard(rawWelcomeCard);
+          await context.sendActivity({ attachments: [card] });
+          break;
+        }
+        case "learn": {
+          this.likeCountObj.likeCount = 0;
+          const card = this.renderAdaptiveCard(rawLearnCard, this.likeCountObj);
+          await context.sendActivity({ attachments: [card] });
+          break;
+        }
+        case "hello": {
+          const card = this.renderAdaptiveCard(rawEscalationsCard);
+          await context.sendActivity({ attachments: [card] });
+          await context.sendActivity(`What can i help you with today?\n 1. EBA Issues.\n 2. TBA Issues.\n Reply with 1/2.`);
+          break;
+        }
+        case "1": {
+          const card = this.renderAdaptiveCard(rawEBACard);
+          await context.sendActivity({ attachments: [card] });
+          const card1 = this.renderAdaptiveCard(rawSMECard);
+          await context.sendActivity({ attachments: [card1] });
+          break;
+        }
+        case "2": {
+          const card = this.renderAdaptiveCard(rawTBACard);
+          await context.sendActivity({ attachments: [card] });
+          const card1 = this.renderAdaptiveCard(rawSMECard);
+          await context.sendActivity({ attachments: [card1] });
+          break;
+        }
+        case "yes": {
+          await context.sendActivity(`Thank God! I checked & it's fine. Here's a TSG to help you with whatever else you need help for.`);
+          const card = this.renderAdaptiveCard(rawWelcomeCard);
+          await context.sendActivity({ attachments: [card] });
+          break;
+        }
+      }
+
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
+    });
+
+    // Listen to MembersAdded event, view https://docs.microsoft.com/en-us/microsoftteams/platform/resources/bot-v3/bots-notifications for more events
+    this.onMembersAdded(async (context, next) => {
+      const membersAdded = context.activity.membersAdded;
+      for (let cnt = 0; cnt < membersAdded.length; cnt++) {
+        if (membersAdded[cnt].id) {
+          const card = this.renderAdaptiveCard(rawWelcomeCard);
+          await context.sendActivity({ attachments: [card] });
+          break;
+        }
+      }
+      await next();
+    });
+  }
+
+  // Invoked when an action is taken on an Adaptive Card. The Adaptive Card sends an event to the Bot and this
+  // method handles that event.
+  async onAdaptiveCardInvoke(context, invokeValue) {
+    // The verb "userlike" is sent from the Adaptive Card defined in adaptiveCards/learn.json
+    if (invokeValue.action.verb === "userlike") {
+      this.likeCountObj.likeCount++;
+      const card = this.renderAdaptiveCard(rawLearnCard, this.likeCountObj);
+      await context.updateActivity({
+        type: "message",
+        id: context.activity.replyToId,
+        attachments: [card],
+      });
+      return { statusCode: 200 };
+    }
+  }
+
+  // Bind AdaptiveCard with data
+  renderAdaptiveCard(rawCardTemplate, dataObj) {
+    const cardTemplate = new ACData.Template(rawCardTemplate);
+    const cardWithData = cardTemplate.expand({ $root: dataObj });
+    const card = CardFactory.adaptiveCard(cardWithData);
+    return card;
+  }
+
+}
+
+
+module.exports.TeamsBot = TeamsBot;
